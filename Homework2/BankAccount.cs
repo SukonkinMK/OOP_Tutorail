564c98b1193014aa3f31b5edac64da5c7bae0613
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2022-07-02 16:04:42 +0300

    Первый пункт ДЗ

---
+++ Homework2/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿namespace Homework2;
+
+public enum Account_Type { 
+    Base,
+    Silver,
+    Gold,
+    Platinum
+}
+
+public class BankAccount
+{
+    private int id;
+    private decimal balance;
+    private Account_Type accountType;
+
+    public int Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+    public decimal Balance
+    {
+        get { return balance; }
+        set { balance = value; }
+    }
+    public Account_Type AccountType
+    {
+        get { return accountType; }
+        set { accountType = value; }
+    }
+    public override string ToString()
+    {
+        return $"Account № {Id}; balance: {Balance}$; type: {AccountType}";
+    }
+}
--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Homework2;
+
+BankAccount bankAccount = new BankAccount();
+bankAccount.Id = 1;
+bankAccount.Balance = 10.50M;
+bankAccount.AccountType = Account_Type.Platinum;
+Console.WriteLine(bankAccount);
+Console.ReadLine();
