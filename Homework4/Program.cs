27839e5cd074850b395f6a0d1d9223dcfc44ace0
albina sukonkina <yagfarovaam28@gmail.com>
2022-07-09 13:10:30 +0300

    Создан класс Building

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Homework4;
+
+Building building1 = new Building();
+Building building2 = new Building(30,9,180,5);
+Console.ReadLine();
