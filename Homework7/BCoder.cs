911c7af92dc59248e403a10c611117a62b590af1
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2022-07-17 17:09:54 +0300

    Выполнил первый пункт 7 ДЗ

---
+++ Homework7/BCoder.cs
@@ -0,0 +1,47 @@
+﻿namespace Homework7;
+
+public class BCoder : IСoder
+{
+    public string Decode(string s)
+    {
+       return Encode(s);
+    }
+
+    public string Encode(string s)
+    {
+        if (s is null)
+        {
+            return null!;
+        }
+        var chars = s.ToCharArray();
+        CodeArray(chars);
+        return new(chars);
+    }
+    private char ShiftLetter(char endAlphabetSymbol, char startAlphabetSymbol, char shiftingLetter)
+    {
+        return (char)(endAlphabetSymbol - (shiftingLetter - startAlphabetSymbol));
+    }
+
+    private void CodeArray (char[] chars)
+    {
+        for (int i = 0; i < chars.Length; i++)
+        {
+            if (chars[i] >= 'A' && chars[i] <= 'Z')
+            {
+                chars[i] = ShiftLetter('Z', 'A', chars[i]);
+            }
+            else if (chars[i] >= 'a' && chars[i] <= 'z')
+            {
+                chars[i] = ShiftLetter('z', 'a', chars[i]);
+            }
+            else if (chars[i] >= 'А' && chars[i] <= 'Я')
+            {
+                chars[i] = ShiftLetter('Я', 'А', chars[i]);
+            }
+            else if (chars[i] >= 'a' && chars[i] <= 'я')
+            {
+                chars[i] = ShiftLetter('я', 'а', chars[i]);
+            }
+        }
+    }
+}
--- Homework7/Program.cs
+++ Homework7/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Homework7;
+
+var a = new ACoder();
+var b = new BCoder();
+string s = "A B C a b c Z z А а Б б Алфавит Alphabet Я я";
+string s_encode1 = a.Encode(s);
+Console.WriteLine(s_encode1);
+string s_decode1 = a.Decode(s_encode1);
+Console.WriteLine(s_decode1);
+
+string s_encode2 = b.Encode(s);
+Console.WriteLine(s_encode2);
+string s_decode2 = b.Decode(s_encode2);
+Console.WriteLine(s_decode2);
+Console.ReadLine();
